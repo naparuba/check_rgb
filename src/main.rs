896a95487e66c5f8f8b7e40bbc6c519b52ebd950
Jean Gabès <naparuba@gmail.com>
2021-07-06 14:12:20 +0200

    Enh: rust code is cleaner

--- src/main.rs
+++ src/main.rs
@@ -1,105 +1,111 @@
-//use std::collections::HashMap;
-extern crate ical;
-extern crate iso8601;
-use chrono::{Datelike, Timelike, Utc};
-use chrono::{DateTime, NaiveDate, NaiveDateTime, NaiveTime};
-use chrono::format::ParseError;
-
-// import ical lib
+use chrono::{Utc};
+use chrono::{NaiveDate, NaiveDateTime};
 use std::io::BufReader;
 
-const DATE_FORMAT_STR: &'static str = "%Y-%m-%d à %H:%M:%S";
+extern crate ical;
+extern crate iso8601;
 
+
+const DATE_FORMAT_STR: &'static str = "%Y-%m-%d à %H:%M:%S";
+const RGB_AGENDA_URI: &'static str = "https://studiorenegade.fr/agenda-5.ics";
+
+#[allow(dead_code)]
 fn print_type_of<T>(_: &T) {
-    //println!("{}", std::any::type_name::<T>())
+    println!("{}", std::any::type_name::<T>())
 }
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    //println!("Hello, world!");
-    let uri = "https://studiorenegade.fr/agenda.ics";
+fn _get_agenda_as_string() -> String {
 
-    let resp = reqwest::blocking::get(uri)?;
-    //.json::<HashMap<String, String>>()?;
-    //println!("{:#?}", resp);
-    let buf = resp.text()?;
-    //println!("{:#?}", buf);
+    let resp = reqwest::blocking::get(RGB_AGENDA_URI);
+    let resp = match resp {
+        Ok(resp) => resp,
+        Err(error) => panic!("Connection à l'agenda impossible: {:?}", error),
+    };
 
-    let bufreader = BufReader::new(buf.as_bytes());
+    let buf = resp.text();
+    let buf = match buf {
+        Ok(buf) => buf,
+        Err(error) => panic!("Ouverture de l'agenda impossible: {:?}", error),
+    };
+
+    return buf;
+}
+
+fn _get_first_date_string_from_agenda(agenda_string: String) -> String {
+    let bufreader = BufReader::new(agenda_string.as_bytes());
     let reader = ical::PropertyParser::from_reader(bufreader);
 
 
     let mut dtstart = String::from("");
     for line in reader {
-        //println!("{:?}", line);
         let prop = line.unwrap();
-
-        //println!("NAME: {:?}", prop.name);
-        if (prop.name == "DTSTART") {
-            //println!("START: {:?}", prop.value);
+        if prop.name == "DTSTART" {
             dtstart = prop.value.unwrap();
+            break;
         }
     }
+    return dtstart;
+}
 
-    if (dtstart == "") {
-        //println!("No RGB planned");
-        return Ok(());
-    }
+fn _get_date_from_datestring(datetime_string: String) -> NaiveDateTime {
+    // Now parse the DTSTART as a real date
+    let datetime = iso8601::datetime(datetime_string.as_str()).unwrap();
 
-    //println!("DTSTART: {:?}", dtstart.as_str());
-    //let rfc3339 = DateTime::parse_from_rfc3339(dtstart.as_str())?;
-    //println!("{:?} : {:?}", dtstart.as_str(), rfc3339);
-    //let datetime = iso8601::datetime("2015-06-26T16:43:23+0200").unwrap();
-    let datetime = iso8601::datetime(dtstart.as_str()).unwrap();
-    //println!("DATE= {:?}", datetime.date);
+    let native_date: NaiveDateTime;
 
-    let native_date : NaiveDateTime;
-    let mut event_epoch = 0;
+    // datetime can be several format (3), only one is managed YMD
     match datetime.date {
-        iso8601::Date::YMD { year,     month, day } => {
-            //println!("YMD format value: {} {} {}", year, month, day);
+        iso8601::Date::YMD { year, month, day } => {
             native_date = NaiveDate::from_ymd(year, month, day).and_hms(datetime.time.hour, datetime.time.minute, datetime.time.second);
-            event_epoch = native_date.timestamp();
-            //println!("Datetime: {}", native_date);
-        },
-        _ => {println!("Something else");
-            return Ok(());
-        },
+        }
+        _ => {
+            println!("<span style='color:red'>ERREUR</span>: le format de date de l'agenda est inconnu");
+            std::process::exit(2);
+        }
     }
-    //println!("TIME= {:?}", datetime.time);
+    return native_date;
+}
 
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Connect to the agenda, and get the whole string of it
+    let agenda_string = _get_agenda_as_string();
 
-    let date_time: NaiveDateTime = NaiveDate::from_ymd(2017, 11, 12).and_hms(17, 33, 44);
-    //println!(
-    //    "Number of seconds between 1970-01-01 00:00:00 and {} is {}.",
-    //    date_time, date_time.timestamp());
+    // Parse the agenda until we get a String of DTSTART
+    let dtstart = _get_first_date_string_from_agenda(agenda_string);
 
+    // Maybe it's void, no RGB :'(
+    if dtstart == "" {
+        println!("Pas de RGB planifié actuellement :'( ");
+        std::process::exit(0);
+    }
+
+    // Parse the datestring, and get a epoch + string representation
+    let event_date = _get_date_from_datestring(dtstart);
+    let event_epoch = event_date.timestamp();
+    let date_str = event_date.format(DATE_FORMAT_STR).to_string();
+
+    // Get the now for comparison
     let now = Utc::now();
     let now_epoch = now.timestamp();
 
-
-    print_type_of(&datetime);
-    //println!("Event in {} ({} - {})", event_epoch - now_epoch, now_epoch, event_epoch);
-    let mut diff_float: f64 = (event_epoch - now_epoch) as f64;
+    // Get the number of days from the event
+    let diff_float: f64 = (event_epoch - now_epoch) as f64;
     let in_nb_days = (diff_float / 86400.0) as i64;
-    //println!("Event in {} days", in_nb_days);
 
-    let date_str = native_date.format(DATE_FORMAT_STR).to_string();
 
+    // Check it! :)
     if in_nb_days <= 1 {
         println!("<span style='color:red'>ALERTE 🎉</span>: le prochain RGB est quasiment là! Tenez vous prêt! [ <span style='color:purple'>{:?}</span> ]", (date_str));
-        return Ok(());
+        std::process::exit(2);
     }
 
 
     if in_nb_days <= 3 {
         println!("<span style='color:orange'>ATTENTION ❗</span>: le prochain RGB est très bientôt! (dans {} jours) Réservez votre soirée! [ <span style='color:purple'>{:?}</span> ]", in_nb_days, date_str);
-        return Ok(());
+        std::process::exit(1);
     }
 
-    if in_nb_days >= 7 {
-        println!("<span style='color:green'>OK</span>: Le prochain RGB est encore loin (dans {} jours), vous avez le temps [ <span style='color:purple'>{:?}</span> ]", in_nb_days, date_str);
-        return Ok(());
-    }
-
-    return Ok(());
+    // It's far away
+    println!("<span style='color:green'>OK</span>: Le prochain RGB est encore loin (dans {} jours), vous avez le temps [ <span style='color:purple'>{:?}</span> ]", in_nb_days, date_str);
+    std::process::exit(0);
 }
